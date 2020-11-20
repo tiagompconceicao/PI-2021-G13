4edda85b8721c8927ba04408dac4def065097016
Bartolomeu <goncalo@example.com>
2020-11-20 20:29:13 +0000

    covida-web-api structure updated, and covida-server routes added

--- covida-server.js
+++ covida-server.js
@@ -4,19 +4,29 @@ aqui é o usado express*/
 
 const PORT = 8000
 
-const express = require('')
+const express = require('express')
 const task  = require('./covida-web-api')
 
 const app = express()
 
 app.use(express.json())
 
-app.get()
+/*app.get('/covida')
 app.get()
 app.get()
 app.delete()
 app.put()
-app.post()
+app.post()*/
+
+app.get('/covida', covida-web-api.getPopularGames)
+app.get(`/covida/game/:name`, covida-web-api.getGameByName)
+app.post(`/covida/groups`, covida-web-api.createGroup)
+app.put(`/covida/groups/:groupName`, covida-web-api.editGroup)
+app.get(`/covida/groups`, covida-web-api.getAllGroups)
+app.get(`/covida/groups/:groupName`, covida-web-api.getGroupDetails)
+app.put(`/covida/groups/:groupName/game/:name`, covida-web-api.addGameToGroup)
+app.delete(`/covida/groups/:groupName/game/:name`, covida-web-api.removeGameFromGroup)
+app.get(`/covida/groups/:groupName/:min/:max`, covida-web-api.getGamesFromGroupWithinRange)
 
 app.listen(PORT, () => {
     console.log(`Tasks app listening at http://localhost:${PORT}`)
--- covida-web-api.js
+++ covida-web-api.js
@@ -1,15 +1,51 @@
 /* criar todas as funcionalidades  */
 
-const tasksDb = require("./covida-services")
+//const tasksDb = require("./covida-services")
 
 let tasks = [0,1,2,3,4,5,6,7,8,9]
 
-tasks = tasks.map((dc, i)=>{
+groups = groups.map((dc, i)=>{
     return {
         id: i,
-        name: `task ${i}`,
-        description: `Description of task ${i}`
+        name: `group ${i}`,
+        description: `Description of group ${i}`
     }
 })
 
 let maxId = tasks.length
+
+
+module.exports = {
+//this functions will call a method from services which fulfills the request (req), 
+//and prepares the response (res), for example with the status code 200 OK
+    getPopularGames: function (req, res){
+        //Obter a lista dos jogos mais populares
+    },
+    getGameByName: function (req, res){
+        //Pesquisar jogos pelo nome
+    },
+    createGroup: function (req, res){
+        //Criar grupo atribuindo-lhe um nome e descrição
+    },
+    editGroup: function (req, res){
+        //Editar grupo, alterando o seu nome e descrição
+    },
+    getAllGroups: function (req,res){
+        //Listar todos os grupos
+    },
+    getGroupDetails: function (req, res){
+        //Listar todos os grupos
+        //Obter os detalhes de um grupo, com o seu nome, descrição e nomes dos jogos que o constituem
+    },
+    addGameToGroup: function (req, res){
+        //Adicionar um jogo a um grupo
+    },
+    removeGameFromGroup: function (req, res){
+        //Remover um jogo de um grupo
+    },
+    getGamesFromGroupWithinRange: function (req, res){
+        //Obter os jogos de um grupo que têm uma votação média (total_rating) entre dois valores 
+        //(mínimo e máximo) entre 0 e 100, sendo estes valores parametrizáveis no pedido. Os jogos 
+        //vêm ordenadas por ordem decrescente da votação média
+    },
+}
