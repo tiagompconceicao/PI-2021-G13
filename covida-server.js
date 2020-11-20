6874a828da9ca6b2b2b564e51cd822bda51be8c5
Bartolomeu <goncalo@example.com>
2020-11-20 20:34:36 +0000

    fixing a mistake from last commit

--- covida-server.js
+++ covida-server.js
@@ -5,7 +5,7 @@ aqui é o usado express*/
 const PORT = 8000
 
 const express = require('express')
-const task  = require('./covida-web-api')
+const webapi  = require('./covida-web-api')
 
 const app = express()
 
@@ -18,15 +18,15 @@ app.delete()
 app.put()
 app.post()*/
 
-app.get('/covida', covida-web-api.getPopularGames)
-app.get(`/covida/game/:name`, covida-web-api.getGameByName)
-app.post(`/covida/groups`, covida-web-api.createGroup)
-app.put(`/covida/groups/:groupName`, covida-web-api.editGroup)
-app.get(`/covida/groups`, covida-web-api.getAllGroups)
-app.get(`/covida/groups/:groupName`, covida-web-api.getGroupDetails)
-app.put(`/covida/groups/:groupName/game/:name`, covida-web-api.addGameToGroup)
-app.delete(`/covida/groups/:groupName/game/:name`, covida-web-api.removeGameFromGroup)
-app.get(`/covida/groups/:groupName/:min/:max`, covida-web-api.getGamesFromGroupWithinRange)
+app.get('/covida', webapi.getPopularGames)
+app.get(`/covida/game/:name`, webapi.getGameByName)
+app.post(`/covida/groups`, webapi.createGroup)
+app.put(`/covida/groups/:groupName`, webapi.editGroup)
+app.get(`/covida/groups`, webapi.getAllGroups)
+app.get(`/covida/groups/:groupName`, webapi.getGroupDetails)
+app.put(`/covida/groups/:groupName/game/:name`, webapi.addGameToGroup)
+app.delete(`/covida/groups/:groupName/game/:name`, webapi.removeGameFromGroup)
+app.get(`/covida/groups/:groupName/:min/:max`, webapi.getGamesFromGroupWithinRange)
 
 app.listen(PORT, () => {
     console.log(`Tasks app listening at http://localhost:${PORT}`)
