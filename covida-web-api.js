860dfad0b30f0aab6a0f5662fec783f72fdbada9
Tiago Conceição <A47611@alunos.isel.pt>
2020-11-22 19:42:19 +0000

    Started reorganization of code

--- covida-web-api.js
+++ covida-web-api.js
@@ -16,36 +16,54 @@ let maxId = groups.length
 
 
 module.exports = {
+    getPopularGames: getPopularGames,
+    getGameByName: getGameByName,
+    createGroup: createGroup,
+    editGroup: editGroup,
+    getAllGroups: getAllGroups,
+    getGroupDetails: getGroupDetails,
+    addGameToGroup: addGameToGroup,
+    removeGameFromGroup: removeGameFromGroup,
+    getGamesFromGroupWithinRange: getGamesFromGroupWithinRange
+}
 //this functions will call a method from services which fulfills the request (req), 
 //and prepares the response (res), for example with the status code 200 OK
-    getPopularGames: function (req, res){
-        //Obter a lista dos jogos mais populares
-    },
-    getGameByName: function (req, res){
-        //Pesquisar jogos pelo nome
-    },
-    createGroup: function (req, res){
-        //Criar grupo atribuindo-lhe um nome e descrição
-    },
-    editGroup: function (req, res){
-        //Editar grupo, alterando o seu nome e descrição
-    },
-    getAllGroups: function (req,res){
-        //Listar todos os grupos
-    },
-    getGroupDetails: function (req, res){
-        //Listar todos os grupos
-        //Obter os detalhes de um grupo, com o seu nome, descrição e nomes dos jogos que o constituem
-    },
-    addGameToGroup: function (req, res){
-        //Adicionar um jogo a um grupo
-    },
-    removeGameFromGroup: function (req, res){
-        //Remover um jogo de um grupo
-    },
-    getGamesFromGroupWithinRange: function (req, res){
-        //Obter os jogos de um grupo que têm uma votação média (total_rating) entre dois valores 
-        //(mínimo e máximo) entre 0 e 100, sendo estes valores parametrizáveis no pedido. Os jogos 
-        //vêm ordenadas por ordem decrescente da votação média
-    }
+
+function getPopularGames(req, res){
+    //Obter a lista dos jogos mais populares
+}
+
+function getGameByName(req, res){
+    //Pesquisar jogos pelo nome
+}
+
+function createGroup(req, res){
+    //Criar grupo atribuindo-lhe um nome e descrição
+}
+
+function editGroup(req, res){
+    //Editar grupo, alterando o seu nome e descrição
+}
+
+function getAllGroups(req,res){
+    //Listar todos os grupos
+}
+
+function getGroupDetails(req, res){
+//Listar todos os grupos
+//Obter os detalhes de um grupo, com o seu nome, descrição e nomes dos jogos que o constituem
+}
+
+function addGameToGroup(req, res){
+    //Adicionar um jogo a um grupo
+}
+     
+function removeGameFromGroup(req, res){
+    //Remover um jogo de um grupo
+}
+     
+function getGamesFromGroupWithinRange(req, res){
+    //Obter os jogos de um grupo que têm uma votação média (total_rating) entre dois valores 
+    //(mínimo e máximo) entre 0 e 100, sendo estes valores parametrizáveis no pedido. Os jogos 
+    //vêm ordenadas por ordem decrescente da votação média
 }
