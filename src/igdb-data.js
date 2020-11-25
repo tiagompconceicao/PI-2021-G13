95aaa905081881c6c8b9bed9b14e498f13196484
Tiago Conceição <A47611@alunos.isel.pt>
2020-11-25 16:11:21 +0000

    Implementation of HTTP requests of igdb-data

--- src/igdb-data.js
+++ src/igdb-data.js
@@ -1,54 +1,92 @@
 //acesso a api igdb 
 //Use UrlLib Module to make http requests
 
-const urllib = require("./urllib")
+//const urllib = require("./node_modules/urllib")
+const urllib = require("urllib")
 
-const token = "Bearer ufcyc2wp04hs7z23ojphz0xujefe13"
-const client_ID = "gvzqbx37hialxnepf7muzm1vx5bqqr"
-const baseUrl = "https://api.igdb.com/v4/games"
+const token = "t19htf57c5iskw9203r5jvm8njhw3j"
+const client_ID = "gvziab9htl4uxx6lanjjf6bqbr16kt"
+const baseUrl = "https://api.igdb.com/v4/games/"
+
+//getGameByName('Fifa 14', null)
+//getPopularGames(null)
 
 module.exports = {
     getPopularGames,
     getGameByName
 }
-
-/*
-urllib.request(baseUrl, {
-  method: 'GET',
-  headers: {
-    'Client-ID': client_ID,
-    'Authorization': `Bearer ${toke}`
-  },
-})
-*/
-
      
 function getPopularGames(cb){
     //Obter a lista dos jogos mais populares
     //todo saber os ulr para fazer request da data necessária
 
-   /*
-        POST https://api.igdb.com/v4/games
-        
-        fields name,rating;
-        sort rating desc;
-        where rating != null;
-    */
+   let get = urllib.request(baseUrl, {
+    method: 'POST',
+    headers: {
+        'Client-ID': client_ID,
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'text/plain'
+    },
+  data: "fields *;sort rating desc;where rating!= null;"
+
+})
+
+//cb(get)
+
+//Remover .then(), penso que não podemos usar esta função Promise
+get.then(result => {
+    console.log(result.status) // "Some User token"
+    const body = JSON.parse(result.data)
+    console.log(body)
+ })
 }
  
-function getGameByName(name, cb){
+async function getGameByName(gameName, cb){
     //Pesquisar jogos pelo nome    
-    //let url =`${baseurl}search?clientId=${client_Id}&authorization=${token}&query=${name}` ????
     //todo saber os ulr para fazer request da data necessária
+    
+    let get = urllib.request(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Client-ID': client_ID,
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'text/plain'
+        },
+       data: `fields name, rating; search "${gameName}"; limit 1;`
+    })
 
-    /*
-    POST https://api.igdb.com/v4/games
-        
-        fields *;
-        where name = name
-    */
+    //Remover .then(), penso que não podemos usar esta função Promise
+    get.then(result => {
+        console.log(result.status) // "Some User token"
+        const body = JSON.parse(result.data)
+        console.log(body)
+     })
+    
 
-    //Also can use: search name;
-    //search returns the exact game name and similar ones
+    //or
+
+    /*let get = urllib.request(baseUrl, {
+        method: 'GET',
+        headers: {
+            'Client-ID': client_ID,
+            'Authorization': `Bearer ${token}`
+        },
+       data:{
+        fields: 'name,rating',
+        search: gameName,
+        limit: 1
+      }
+    
+    })
+
+    //cb(get)
+
+    //Remover .then(), penso que não podemos usar esta função Promise
+    get.then(result => {
+        console.log(result.status) // "Some User token"
+        const body = JSON.parse(result.data)
+        console.log(body)
+     })*/
+
 }
 
